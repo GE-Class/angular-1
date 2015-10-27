40eed990c4b50975d562eab57e787f7bfa79bdaf
Chyld Medford <chyld.medford@gmail.com>
2015-10-27 11:04:02 -0700

    wip 1

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 angular.module('ge', [])
 .controller('main', function($scope){
   $scope.x = 3;
+  $scope.y = 2 * 11;
+  $scope.numbers = [];
+  $scope.add = function(){
+    $scope.numbers.push(this.z);
+  };
 });
